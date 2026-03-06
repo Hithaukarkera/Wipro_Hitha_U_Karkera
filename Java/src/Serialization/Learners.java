9bdc85ef053d237576c5d9440dd384bdd20a1567
Hitha U Karkera <hithaumesh0708@gmail.com>
2026-03-06 17:15:35 +0530

    Add files via upload

---
+++ Java/src/Serialization/Learners.java
@@ -0,0 +1,56 @@
+package Serialization;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.Scanner;
+
+public class Learners implements Serializable{
+	int id;
+	String name;
+	double marks;
+	Learners(int id, String  name, double marks){
+		this.id=id;
+		this.name=name;
+		this.marks=marks;
+	}
+	
+	void display() {
+		System.out.println("ID" +id);
+		System.out.println("Name" +name);
+		System.out.println("Marks" +marks);
+	}
+	
+
+	public static void main(String[] args) {
+		try {
+			Scanner sc= new Scanner(System.in);
+			
+			System.out.println("Enter ID: ");
+			int id = sc.nextInt();
+			sc.nextLine();
+			System.out.println("Enter Name");
+			String name= sc.nextLine();
+			
+			System.out.println("Enter Marks");
+			double marks = sc.nextDouble();
+			
+			Learners l1= new Learners(id, name, marks);
+			
+			FileInputStream fis= new FileInputStream("Learner.txt");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			
+			Learners l2 =  (Learners) ois.readObject();
+			ois.close();
+			fis.close();
+			
+			System.out.println("Deserialized Object Data");
+			l2.display();
+			
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
+
+}
